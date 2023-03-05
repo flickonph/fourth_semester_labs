e7f9af3e66347195b99332e12e6e2d05be1655e6
Vladimir Gorobets <flickonph@gmail.com>
2023-03-06 02:00:30 +0300

    dmpcs_labs: hotfix

---
+++ dmpcs_labs/dmpcs/Controller.cs
@@ -0,0 +1,24 @@
+﻿namespace dmPCs;
+
+internal static class Program
+{
+	internal static int Main()
+	{
+		int i = Convert.ToInt32(Console.ReadLine());
+		switch (i)
+		{
+			case 1:
+				Lab1.Lab1Main();
+				break;
+			case 2:
+				Lab2.Lab2Main();
+				break;
+			default:
+				Console.WriteLine("Doesn't exist");
+				break;
+		}
+
+		Console.ReadKey();
+		return 0;
+	}
+}
