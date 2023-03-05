c40e2cd2aebfadfc7c3a5f3fc85512873a02fbbc
Vladimir Gorobets <flickonph@gmail.com>
2023-03-06 01:12:45 +0300

    prog_tech_labs: completed fourth lab

--- prog_tech_labs/program_tech_labs/Models/Lab4Model.cs
+++ prog_tech_labs/program_tech_labs/Models/Lab4Model.cs
@@ -7,20 +7,48 @@ namespace program_tech_labs.Models;
 
 public static class Lab4Model
 {
-    public static List<UrlInfo> GetTopLevelDomains()
+    public static List<UrlInfo> GetDomainsInfo()
     {
         var xmlReader = new XmlReader("Assets/Resources.xml");
         List<string> paths = xmlReader.GetPaths();
-        List<UrlInfo> domains = new List<UrlInfo>();
-        foreach (var path in paths)
+        List<UrlInfo> urls = new List<UrlInfo>();
+        for (var index = 0; index < paths.Count; index++)
         {
+            var path = paths[index];
             var uri = new Uri(path);
             var domain = uri.Host.Split(".").Last();
-            
-            domains.Add(new UrlInfo(path, domain, 0));
+            var t = uri.LocalPath;
+            int start = t.LastIndexOf("/", StringComparison.Ordinal);
+            int length = t.Length - start;
+            t = t.Remove(start, length);
+            if (t.Length > 0)
+            {
+                path = paths[index].Replace(t, $"/NEW_ROOT_PATH_{index + 1}");
+            }
+
+            urls.Add(new UrlInfo(path, domain, 0));
         }
 
-        return domains;
+        int urlsCount = urls.Count;
+
+        List<string> tempPaths = new List<string>();
+        List<int> foldingLevels = new List<int>();
+        foreach (var urlInfo in urls)
+        {
+            string temp = urlInfo.Path.Remove(0, 8);
+            int start = temp.IndexOf('/');
+            int length = temp.Length - start;
+            tempPaths.Add(temp.Remove(start, length));
+            foldingLevels.Add(temp.Count(f => f == '.'));
+        }
+
+        for (int i = 0; i < urlsCount; i++)
+        {
+            urls[i].Domain = tempPaths[i];
+            urls[i].FoldingLevel = foldingLevels[i];
+        }
+
+        return urls;
     }
 }
 
@@ -41,16 +69,26 @@ public class XmlReader
     }
 }
 
-public struct UrlInfo
+public class UrlInfo
 {
     public UrlInfo(string path, string topLevelDomain, int foldingLevel)
     {
         Path = path;
+        Domain = string.Empty;
         TopLevelDomain = topLevelDomain;
         FoldingLevel = foldingLevel;
     }
 
     public string Path { get; }
+    public string Domain { get; set; }
     public string TopLevelDomain { get; }
-    public int FoldingLevel { get; }
+    public int FoldingLevel { get; set; }
+
+    public override string ToString()
+    {
+        return $"Absolute path (renamed) : {Path}\n" +
+               $"Domain: {Domain}\n" +
+               $"Top level domain: {TopLevelDomain}\n" +
+               $"Folding level: {FoldingLevel}";
+    }
 }
--- prog_tech_labs/program_tech_labs/Views/Pages/Lab4Page.xaml.cs
+++ prog_tech_labs/program_tech_labs/Views/Pages/Lab4Page.xaml.cs
@@ -1,18 +1,28 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Linq;
 using program_tech_labs.Models;
+using Wpf.Ui.Controls;
 
 namespace program_tech_labs.Views.Pages;
 
 public partial class Lab4Page
 {
+    private readonly List<UrlInfo> _domains;
     public Lab4Page()
     {
         InitializeComponent();
-        Lab4Model.GetTopLevelDomains();
+        _domains = Lab4Model.GetDomainsInfo();
+        Lab4Main();
     }
 
-    public static void Lab4Main()
+    private void Lab4Main()
     {
-        
+        foreach (var card in _domains.Select(domain => new Card
+                 {
+                     Content = domain
+                 }))
+        {
+            DomainsListBox.Items.Add(card);
+        }
     }
 }
